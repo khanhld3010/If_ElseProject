1453b338e8d5471eec00c56f488029ff111e25ad
khanhld3010 <luuduckhanh3010@gmail.com>
2026-03-12 09:04:40 +0700

    luyen tap 1 if else

--- jsFile/Examples.js
+++ jsFile/Examples.js
@@ -31,6 +31,63 @@ function example3() {
     hiddenText3.innerHTML = "Số ngẫu nhiên: "+randomNumber + "<br/>" + positiveNumberMessage;
 }
 
+function example4() {
+    let hiddenText4 = document.getElementById("hiddenText4");
+    let number1 = +document.getElementById("number1ex4").value;
+    let number2 = +document.getElementById("number2ex4").value;
+    let number3 = +document.getElementById("number3ex4").value;
+
+    if (number1 > number2 && number1>number3){
+        hiddenText4.innerHTML = "Số lớn nhất là: "+number1;
+    }else if(number2>number1 && number2>number3){
+        hiddenText4.innerHTML = "Số lớn nhất là: "+number2;
+    }else{
+        hiddenText4.innerHTML = "Số lớn nhất là: "+number3;
+    }
+}
+
+function example5() {
+    let hiddenText5 = document.getElementById("hiddenText5");
+    let number1 = +document.getElementById("number1ex5").value;
+    let number2 = +document.getElementById("number2ex5").value;
+    let number3 = +document.getElementById("number3ex5").value;
+    let average = (number3+number2+number1)/3;
+    if (average >= 9.0){
+        hiddenText5.innerHTML = "Bạn xếp hạng A"
+    }else if (average >= 7.0 && average < 9.0)
+    {
+        hiddenText5.innerHTML = "Bạn xếp hạng B"
+    }else if (average >= 5.0 && average < 7.0){
+        hiddenText5.innerHTML = "Bạn xếp hạng C"
+    }else {
+        hiddenText5.innerHTML = "Bạn xếp hạng D"
+    }
+}
+
+function example6(){
+    let hiddenText6 = document.getElementById("hiddenText6");
+    let number1 = +document.getElementById("number1ex6").value;
+    let phanTramHoaHong = 0.05;
+    let soTienNhanDuoc = number1*phanTramHoaHong;
+    hiddenText6.innerHTML = "Số tiền nhận được của bạn là: "+soTienNhanDuoc;
+}
+
+function example7(){
+    let hiddenText7 = document.getElementById("hiddenText7");
+    let number1 = +document.getElementById("number1ex7").value;
+    let chiPhiCoDinh = 25000;
+    let soTienNhanDuoc;
+    if (number1 > 200){
+        soTienNhanDuoc = 50*600 + 400*150 + (number1-200)*200
+    }else if (number1 > 50){
+        soTienNhanDuoc = (number1-50)*400 + 50*600;
+    }else{
+        soTienNhanDuoc = number1*600;
+    }
+
+    hiddenText7.innerHTML = "Số tiền cần trả là: "+(soTienNhanDuoc+chiPhiCoDinh);
+}
+
 function laySoNgauNhien(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
